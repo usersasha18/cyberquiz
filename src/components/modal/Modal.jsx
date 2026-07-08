9734eddbfdfdf47dbc2e58fab8157fb47e94389e
Александр <aleksandr@Noutbuk-Aleksandr.local>
2026-07-08 21:10:08 +1000

    Updates

--- src/components/modal/Modal.jsx
+++
@@ -1,8 +0,0 @@
-import { useGLTF } from "@react-three/drei";
-import modelUrl from "../../assets/Untitled1.glb";
-
-export default function Modal() {
-  const { scene } = useGLTF(modelUrl);
-
-  return <primitive object={scene} scale={4} />;
-}
